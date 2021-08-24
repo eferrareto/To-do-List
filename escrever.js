ab827ac5619cf1e1953cbccad3d7c761f80e0da2
Erick Ferrareto <e.ferraretowork@gmail.com>
2021-08-24 10:13:04 -0300

    inserindo funções em JS

---
+++ escrever.js
@@ -0,0 +1,7 @@
+let text = document.getElementById('texto')
+function escrever(){
+    document.getElementById('resultado').innerHTML += (`<ul><li>${text.value}</li></ul>`)
+}
+function excluir(){
+    document.getElementById('resultado').innerHTML = ''
+}
