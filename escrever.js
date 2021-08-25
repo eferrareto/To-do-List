bd9f53feb661d533ac76871a27b042cf5fa769d9
Erick Ferrareto <e.ferraretowork@gmail.com>
2021-08-25 17:37:34 -0300

    commit

--- escrever.js
+++ escrever.js
@@ -1,7 +1,16 @@
 let text = document.getElementById('texto')
 function escrever(){
-    document.getElementById('resultado').innerHTML += (`<ul><li>${text.value}.</li></ul>`)
+    let lista = document.getElementsByTagName('ul')[0];
+    let l = document.createElement('li');
+    
+    lista.appendChild(l);
+    let checagem = document.createElement('input');
+    checagem.type = 'checkbox'
+    l.appendChild(checagem);
+    l.innerHTML += `${text.value}`
+    
 }
 function excluir(){
+    document.reload
     document.getElementById('resultado').innerHTML = ''
 }
